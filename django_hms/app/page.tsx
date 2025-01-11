ef4330640d76be8730ae3ddee499c377c6216063
alexeylapachukStudent <129609640+alexeylapachukStudent@users.noreply.github.com>
2025-01-11 19:37:32 +0100

    Installed and set up next.js

---
+++ django_hms/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+
+      <main className="">
+        Django
+        <h2 className="text-PWRoom">DJANGO and next rules</h2>
+      </main>
+  );
+}
